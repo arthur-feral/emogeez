92e91d646c350f421ce9f88d0839948dc9703338
arthur-feral <arthur.feral@hotmail.fr>
2018-04-07 09:40:11 +0200

    feat(Http): add get method

--- packages/emogeez-parser/lib/http/http.js
+++ packages/emogeez-parser/lib/http/http.js
@@ -1,5 +1,34 @@
 import 'whatwg-fetch';
 
+/**
+ *
+ * @param config
+ * @return {{get: function(*)}}
+ */
 export default (config) => {
 
+
+  /**
+   *
+   * @param themeName
+   */
+  const get = (themeName) => {
+    const url = `${config.themesUrl}/${themeName}/${themeName}.json`;
+
+    return fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response;
+        }
+
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      })
+      .then((response) => response.json());
+  };
+
+  return {
+    get,
+  };
 };
