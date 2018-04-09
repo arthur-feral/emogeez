da2ad035d08ee45f4c8b7378d9cc3330b06d710d
arthur-feral <arthur.feral@hotmail.fr>
2018-04-09 14:51:55 +0200

    fix resources url

--- packages/emogeez-parser/lib/constants.js
+++ packages/emogeez-parser/lib/constants.js
@@ -1,2 +1,2 @@
 export const DEFAULT_THEME_NAME = 'apple';
-export const DEFAULT_THEMES_URL = '//cdn.jsdelivr.net/gh/arthur-feral/emogeez-generator@{{version}}/emojis';
+export const DEFAULT_THEMES_URL = 'https://cdn.jsdelivr.net/gh/arthur-feral/emogeez@v{{version}}/emogeez-generator/emojis/';
