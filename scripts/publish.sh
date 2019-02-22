2902983f3279a9d61f577cb415eace307d975eef
arthur-feral <arthur.feral@hotmail.fr>
2019-02-22 14:59:43 +0100

    fix publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,14 +12,14 @@ if [[ " ${ALLOWED_VALUES[*]} " != *" $VERSION "* ]]; then
     exit 1;
 fi
 
+echo "preparing $VERSION"
 
-yarn run lerna version $VERSION --exact --force-publish=* -m \"misc: lerna version %v\" --no-git-tag-version --no-push --yes
+yarn run lerna version $VERSION --force-publish=* --exact --no-push -m "misc: lerna version %v" --yes
 NEW_VERSION=$(node -p -e "require('./lerna.json').version")
 echo "publishing $VERSION $NEW_VERSION"
 
 find $(pwd)/packages/emogeez-generator/emojis -type f -name "*.*css" -exec sed -E -i '' 's/emogeez@(([0-9]+(\.[0-9]+)+)|latest)/emogeez@'"$NEW_VERSION"'/g' {} +
 git add -A
-git commit -m "misc: publish %v"
-git push origin/master
-
-yarn run lerna publish --yes --dist-tag v$NEW_VERSION
+git commit -m "misc: update assets files with version $NEW_VERSION"
+git push
+yarn run lerna publish from-package --force-publish=* --yes
